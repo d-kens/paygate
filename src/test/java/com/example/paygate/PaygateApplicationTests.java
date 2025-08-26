fa9a40e809fbacbeba8ceb86b7a1ec0f0d14cc56
d-kens <omondikens1@gmail.com>
2025-08-26 05:11:35 +0300

    Initial commit

---
+++ src/test/java/com/example/paygate/PaygateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.paygate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaygateApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
